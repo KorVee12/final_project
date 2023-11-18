04ee8cca7a7df981ba82c3bdd4ef05efaee703f2
KorVee12 <skybluekor@gmail.com>
2023-11-19 01:56:02 +0700

    Handle person and login

--- database.py
+++ database.py
@@ -2,18 +2,70 @@
 
 import csv, os
 
-__location__ = os.path.realpath(
-    os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-persons = []
-with open(os.path.join(__location__, 'persons.csv')) as f:
-    rows = csv.DictReader(f)
-    for r in rows:
-        persons.append(dict(r))
-print(persons)
+def get_persons(filename_csv="persons.csv"):
+    __location__ = os.path.realpath(
+        os.path.join(os.getcwd(), os.path.dirname(__file__))
+    )
+    persons = []
+    with open(os.path.join(__location__, filename_csv)) as f:
+        rows = csv.DictReader(f)
+        for r in rows:
+            persons.append(dict(r))
+    return persons
+
 
 # add in code for a Database class
 
+
+class Database:
+    def __init__(self, name_db):
+        self.name = name_db
+        self.tables = []
+
+    def add_table(self, table_object):
+        self.tables.append(table_object)
+
+    def get_tables(self):
+        return self.tables
+
+
 # add in code for a Table class
+class Table:
+    def __init__(self, table_name):
+        self.name = table_name
+        self.__list_dict = []
+        self.__fields = []
+
+    def add_fields(self, data: list):
+        self.__fields = data
+
+    def add_row(self, data: list):
+        for row in data:
+            for field in self.__fields:
+                try:
+                    if row[field]:
+                        continue
+                except:
+                    raise Exception(f"Not found field '{field}'")
+
+            self.__list_dict.append(row)
+
+    def __repr__(self) -> str:
+        return f"Tables {self.name}"
+
+
+if __name__ == "__main__":
+    db = Database("test")
+    table = Table("person")
+    table.add_fields(["ID", "fists", "last", "type"])
+    person = [
+        {"ID": "7525643", "fist": "Henrikh", "last": "Mkhitaryan", "type": "faculty"},
+        {"ID": "2472659", "fist": "Karim", "last": "Benzema", "type": "faculty"},
+    ]
+    table.add_row(person)
+    # db.add_table(table)
+    # print(db.get_tables())
+    # print(get_persons())
 
 # modify the code in the Table class so that it supports the insert operation where an entry can be added to a list of dictionary
--- project_manage.py
+++ project_manage.py
@@ -1,17 +1,67 @@
 # import database module
+from database import Database, Table, get_persons
+import copy
+import random
+import string
 
 # start by adding the admin related code
+db = Database("project_manage")
+
 
 # create an object to read an input csv file, persons.csv
+class Persons:
+    def __init__(self):
+        self.persons = get_persons("persons.csv")
+
+
+persons = Persons().persons
+
 
 # create a 'persons' table
+persons_table = Table("persons")
+persons_table.add_fields(["ID", "fist", "last", "type"])
+persons_table.add_row(persons)
+
 
 # add the 'persons' table into the database
+db.add_table(persons_table)
+
 
 # create a 'login' table
+login_table = Table("login")
+
 
 # the 'login' table has the following keys (attributes):
+login_table.add_fields(["person_id", "username", "password", "role"])
 
+
+def change_name_in_dict(input_dict):
+    output_dict = copy.deepcopy(input_dict)
+    for item in output_dict:
+        item["person_id"] = item.pop("ID")
+        item["username"] = item.pop("fist")
+        item["password"] = item.pop("last")
+        item["role"] = item.pop("type")
+
+    return output_dict
+
+
+def random_string(length):
+    characters = string.ascii_lowercase + string.digits
+    result = "".join(random.choice(characters) for i in range(length))
+    return result
+
+
+def change_username_password(login_member):
+    for i in login_member:
+        i["username"] = i["username"] + "." + i["password"][0]
+        i["password"] = random_string(4)
+    return login_member
+
+
+login_member = change_name_in_dict(persons)
+login_member = change_username_password(login_member)
+print(login_member)
 # person_id
 # username
 # password
